cc91a074a71350f2e2effb5c090547fa02a1aaa5
Rohit Bhat <rohitwritescode@gmail.com>
2021-09-19 21:26:05 -0700

    Completed initial layout of index.html and style.css. Currently working through script.js.

---
+++ Day-18_Background-Slider/script.js
@@ -0,0 +1,18 @@
+const body = document.body;
+const slides = document.querySelectorAll('.slide');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+// console.log(slides)
+
+body.style.backgroundImage
+
+let activeSlide = 0;
+const test = slides[activeSlide].style.backgroundImage
+console.log(test)
+
+// setBgToBody()
+
+// function setBgToBody() {
+//     let test = slides[activeSlide].style.backgroundImage
+//     console.log(test)
+// }
